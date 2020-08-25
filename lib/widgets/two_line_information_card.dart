2ec04915de0e6847e307065ce7d692de40b7df81
Byzzee <63951322+Byzzee@users.noreply.github.com>
2020-08-25 08:48:07 +0300

    Добавил ассеты оружия, а также исправил переполнение карточки

--- lib/widgets/two_line_information_card.dart
+++ lib/widgets/two_line_information_card.dart
@@ -23,22 +23,24 @@ class _TwoLineInformationCardState extends State<TwoLineInformationCard> {
         child: Row(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Padding(
-                  padding: EdgeInsets.only(top: 10.0, left: 16.0, right: 16.0, bottom: 10.0),
-                  child: Column(
-                      mainAxisAlignment:MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Padding(
-                            padding: EdgeInsets.only(bottom: 4.0),
-                            child: Opacity(
-                                opacity: 0.3,
-                                child: Text(_topText, style: appTheme.textTheme.subtitle2)
-                            )
-                        ),
-                        Text(_bottomText, style: appTheme.textTheme.headline2)
-                      ]
-                  )
+              Expanded(
+                child: Padding(
+                    padding: EdgeInsets.only(top: 10.0, left: 16.0, right: 16.0, bottom: 10.0),
+                    child: Column(
+                        mainAxisAlignment:MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                              padding: EdgeInsets.only(bottom: 4.0),
+                              child: Opacity(
+                                  opacity: 0.3,
+                                  child: Text(_topText, style: appTheme.textTheme.subtitle2)
+                              )
+                          ),
+                          Text(_bottomText, style: appTheme.textTheme.headline2, overflow: TextOverflow.fade,)
+                        ]
+                    )
+                )
               )
             ]
         )
